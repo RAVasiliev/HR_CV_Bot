de87d909367d2bf56559166b95d774f57d1f9b80
RAVasiliev <41761464+RAVasiliev@users.noreply.github.com>
2023-06-02 16:07:28 +0300

    Update main_hr.py

--- main_hr.py
+++ main_hr.py
@@ -1,9 +1,12 @@
-import telebot 
-import PyPDF2
-import urllib.request
+#  Copyright (c) ChernV (@otter18), 2021.
+
+import os
+import random
+import telebot
 import openai 
 
-TG_TOKEN = '6093361965:AAEYt6M4JSZcpLrGjC0tqaTExLYQqbXQNIQ'
+bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
+
 openai.api_key = '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'
 
 def generate_response(text):
@@ -20,53 +23,40 @@ def generate_response(text):
     return response['choices'][0]['text']
 
 
-
-def pdf_to_text(pdf_path):
-    # Открываем PDF файл в режиме чтения бинарного файла
-    with open(pdf_path, 'rb') as file:
-        # Создаем объект PDFReader
-        pdf_reader = PyPDF2.PdfReader(file)
-
-        # Переменная для хранения текста из PDF
-        text = ""
-
-        # Проходимся по всем страницам PDF
-        for page_num in range(len(pdf_reader.pages)):
-            # Получаем объект страницы
-            page = pdf_reader.pages[page_num]
-
-            # Извлекаем текст из страницы
-            page_text = page.extract_text()
-
-            # Добавляем текст из текущей страницы в общий текст
-            text += page_text
-
-    # Возвращаем извлеченный текст
-    return text
-
-bot = telebot.TeleBot(TG_TOKEN)
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Hi! I am an HR bot that will help you make your CV flawless! \
+    The commands available now are: /about, /experience, /skills. Send me one of these tags and the corresponding CV block in one message and together we will make it better :)")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    if '/about' in message.text:
+        prompt = 'Imagine you are a professional, highly skilled HR, and you help me to fix an "About me" that summarises all your experience in a short, attractive, proofed, valuable, and sellable bio with the highlighted results in numbers with symbol limitation in 500 characters. The main goal is to attract the top HRs from IT companies. Here is the pattern that you should upgrade: '
+        prompt_w_message = prompt + message.text
+        try:
+            bot.reply_to(message, generate_response(prompt_w_message))
+        except: 
+            bot.reply_to(message, 'Smth wrong')
+    elif '/experience' in message.text:
+        prompt = 'Imagine you are a professional highly skilled HR and you help me to rewrite my work experience in a resume based on the STAR method (without dividing by STAR) with achieved results in numbers, each line not longer than 300 symbols. Use direct action like "I did". The main goal is to attract the top HRs from IT companies. Here is the pattern that you should upgrade: '
+        prompt_w_message = prompt + message.text
+        try:
+            bot.reply_to(message, generate_response(prompt_w_message))
+        except: 
+            bot.reply_to(message, 'Smth wrong')
+    elif '/skills' in message.text:
+        prompt = 'Imagine you are a professional, highly skilled HR, and you help me to fix the below list of skills  (20 skills) and make my CV appear in the LinkedIn HR search on the top position. Provide the list of LinkedIn skills in the same format on each new line without other text. Here is the pattern that you should upgrade: '
+        prompt_w_message = prompt + message.text
+        try:
+            bot.reply_to(message, generate_response(prompt_w_message))
+        except: 
+            bot.reply_to(message, 'Smth wrong')
 
-@bot.message_handler(content_types=['document', 'audio'])
-def handle_docs_audio(message):
-    file_info = bot.get_file(message.document.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    src = '/Users/ravasiliev/Documents/HR Bot/data/' + message.document.file_name;
-    with open(src, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    
-    file_text = pdf_to_text(src)
-    response_to_user = generate_response('привет, что происходит в тексте далее? текст: ' + file_text[:1000])
-
-    
-    bot.reply_to(message, response_to_user)
+    else:
+        bot.reply_to(message, 'Please, send me message like that: [/about; /experience; /skills] + [Your pattern]. For example, "/about Middle DA, worked in 2 corporations"' + message.text)
 
 
-bot.infinity_polling()
+
+# ---------------- local testing ----------------
+if __name__ == '__main__':
+    bot.infinity_polling()
